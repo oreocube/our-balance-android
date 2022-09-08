77ad0a9d07467fa6a5ca42ef0db21040facfc21d
oreocube <oreocube13@gmail.com>
2022-09-09 00:28:09 +0900

    chore: 중복되는 gradle setting 파일로 분리 #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,37 +5,6 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
-android {
-    compileSdk = Config.COMPILE_SDK
-
-    defaultConfig {
-        applicationId = Config.APPLICATION_ID
-        minSdk = Config.MIN_SDK
-        targetSdk = Config.TARGET_SDK
-        versionCode = Config.VERSION_CODE
-        versionName = Config.VERSION_NAME
-
-        testInstrumentationRunner = Config.TEST_INSTRUMENTATION_RUNNER
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
 dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -5,25 +5,6 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
-android {
-    compileSdk = Config.COMPILE_SDK
-    defaultConfig {
-        minSdk = Config.MIN_SDK
-        targetSdk = Config.TARGET_SDK
-        testInstrumentationRunner = Config.TEST_INSTRUMENTATION_RUNNER
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-}
-
 dependencies {
     implementation(project(":domain"))
 

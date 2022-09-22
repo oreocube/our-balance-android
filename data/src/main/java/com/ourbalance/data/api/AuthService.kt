da5cf5376c130fff6020cab1e196e12312cdc924
oreocube <oreocube13@gmail.com>
2022-09-22 16:14:42 +0900

    feat: AuthService 추가 #5

---
+++ data/src/main/java/com/ourbalance/data/api/AuthService.kt
@@ -0,0 +1,14 @@
+package com.ourbalance.data.api
+
+import com.ourbalance.data.entity.BaseResponse
+import com.ourbalance.data.entity.UserInfoEntity
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("/user/signup")
+    suspend fun signup(@Body userInfoEntity: UserInfoEntity): BaseResponse<Unit>
+
+    @POST("/user/login")
+    suspend fun login(@Body userInfoEntity: UserInfoEntity): BaseResponse<String>
+}

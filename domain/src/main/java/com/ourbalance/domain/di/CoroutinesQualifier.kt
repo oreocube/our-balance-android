54a2d392e34103d1ec664fc3507f4ca47947aaf1
oreocube <oreocube13@gmail.com>
2022-09-22 16:11:32 +0900

    feat: Coroutine Qualifier 정의 #5

---
+++ domain/src/main/java/com/ourbalance/domain/di/CoroutinesQualifier.kt
@@ -0,0 +1,15 @@
+package com.ourbalance.domain.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher

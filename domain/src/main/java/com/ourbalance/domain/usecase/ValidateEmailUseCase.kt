efaf250755d920ee43e3ffd9ae0e2d091fd123ef
주지혜 <oreocube13@gmail.com>
2022-09-23 16:46:55 +0900

    fix: ktlint

--- domain/src/main/java/com/ourbalance/domain/usecase/ValidateEmailUseCase.kt
+++ domain/src/main/java/com/ourbalance/domain/usecase/ValidateEmailUseCase.kt
@@ -9,8 +9,8 @@ class ValidateEmailUseCase @Inject constructor() {
             "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
             "(" +
             "\\." +
-          "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-          ")+"
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+"
     )
 
     operator fun invoke(input: String): Boolean {

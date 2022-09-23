c69704016e9c8ebb6979ac434c3c5648b6002262
주지혜 <oreocube13@gmail.com>
2022-09-23 16:45:52 +0900

    fix: ktlint

--- domain/src/main/java/com/ourbalance/domain/usecase/ValidateEmailUseCase.kt
+++ domain/src/main/java/com/ourbalance/domain/usecase/ValidateEmailUseCase.kt
@@ -5,12 +5,12 @@ import javax.inject.Inject
 class ValidateEmailUseCase @Inject constructor() {
     private val emailRegex = Regex(
         "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-        "\\@" +
-        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-        "(" +
-        "\\." +
-        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-        ")+"
+            "\\@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+          "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+          ")+"
     )
 
     operator fun invoke(input: String): Boolean {

74057f4176b8ead612100dbc9e821b2576f41525
oreocube <oreocube13@gmail.com>
2022-09-25 17:46:12 +0900

    feat: 새로운 방 생성 기능 구현 #17

--- data/src/main/java/com/ourbalance/data/source/remote/balance/BalanceDataSourceImpl.kt
+++ data/src/main/java/com/ourbalance/data/source/remote/balance/BalanceDataSourceImpl.kt
@@ -1,9 +1,11 @@
 package com.ourbalance.data.source.remote.balance
 
 import com.ourbalance.data.api.BalanceService
+import com.ourbalance.data.entity.mapper.toEntity
 import com.ourbalance.data.entity.mapper.toModel
 import com.ourbalance.domain.model.BalanceDetail
 import com.ourbalance.domain.model.BalanceInfo
+import com.ourbalance.domain.model.RoomInfo
 import javax.inject.Inject
 
 class BalanceDataSourceImpl @Inject constructor(
@@ -17,5 +19,9 @@ class BalanceDataSourceImpl @Inject constructor(
 
     override suspend fun getBalanceDetail(id: Long): BalanceDetail {
         return balanceService.getBalanceDetail(id).toModel()
+    }
+
+    override suspend fun addBalance(roomInfo: RoomInfo): Long {
+        return balanceService.createBalance(roomInfo.toEntity()).roomId
     }
 }

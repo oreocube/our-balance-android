b0fde7ee58d3d3ad031cf8a8a45294cbeff676e9
oreocube <oreocube13@gmail.com>
2022-09-28 11:13:25 +0900

    feat: 로그인 전 기본 메인화면 구현 #21

---
+++ feature/src/main/java/com/ourbalance/feature/ext/ToastExtension.kt
@@ -0,0 +1,8 @@
+package com.ourbalance.feature.ext
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}

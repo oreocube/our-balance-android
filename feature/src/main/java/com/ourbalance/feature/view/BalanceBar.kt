84d7fe7a234db8801c0f88b625d8eb45681b93be
oreocube <oreocube13@gmail.com>
2022-10-05 14:10:52 +0900

    ui: BalanceBar 커스텀 뷰 구현 #29

---
+++ feature/src/main/java/com/ourbalance/feature/view/BalanceBar.kt
@@ -0,0 +1,101 @@
+package com.ourbalance.feature.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.ColorInt
+import com.ourbalance.feature.R
+
+class BalanceBar constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+    private lateinit var borderRect: RectF
+    private lateinit var ratioRect: RectF
+    private lateinit var borderPath: Path
+    private lateinit var ratioPath: Path
+
+    private val cornerRadius = 32f
+    private var fillColor: Int = Color.YELLOW
+    private var ratio = 0
+    private var w: Int = 0
+    private var h: Int = 0
+
+    private val strokePaint: Paint = Paint(ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = 6f
+        color = Color.BLACK
+    }
+
+    private val paint: Paint = Paint(ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = fillColor
+    }
+
+    init {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.BalanceBar, 0, 0).apply {
+            try {
+                fillColor = getColor(R.styleable.BalanceBar_fillColor, 0)
+                ratio = getInteger(R.styleable.BalanceBar_ratio, 0)
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    fun setFillColor(@ColorInt color: Int) {
+        fillColor = color
+        invalidate()
+    }
+
+    fun setRatio(ratio: Int) {
+        this.ratio = ratio
+        drawRatioRect(ratio)
+        invalidate()
+    }
+
+    private fun drawRatioRect(ratio: Int) {
+        ratioRect =
+            RectF(0f, 0f, w.toFloat() * ratio / 100, h.toFloat()).apply {
+                inset(strokePaint.strokeWidth / 2, strokePaint.strokeWidth / 2)
+            }
+
+        ratioPath = Path().apply {
+            addRect(ratioRect, Path.Direction.CW)
+            op(borderPath, Path.Op.INTERSECT)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        this.w = w
+        this.h = h
+
+        borderRect = RectF(0f, 0f, w.toFloat(), h.toFloat()).apply {
+            inset(strokePaint.strokeWidth / 2, strokePaint.strokeWidth / 2)
+        }
+
+        borderPath = Path().apply {
+            addRoundRect(borderRect, cornerRadius, cornerRadius, Path.Direction.CW)
+        }
+
+        drawRatioRect(ratio)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        with(canvas) {
+            drawPath(ratioPath, paint)
+            drawPath(ratioPath, strokePaint)
+            drawPath(borderPath, strokePaint)
+        }
+    }
+}

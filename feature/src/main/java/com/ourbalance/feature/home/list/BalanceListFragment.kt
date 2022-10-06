fbdf3b054ca355f7e8e4e2b51c9a2c0e4105a73d
oreocube <oreocube13@gmail.com>
2022-10-06 11:35:48 +0900

    ui: 밸런스 항목 추가 바텀시트 레이아웃 구현  #31

--- feature/src/main/java/com/ourbalance/feature/home/list/BalanceListFragment.kt
+++ feature/src/main/java/com/ourbalance/feature/home/list/BalanceListFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.ourbalance.feature.constant.ADD_BALANCE_BOTTOM_SHEET
 import com.ourbalance.feature.databinding.FragmentBalanceListBinding
 import com.ourbalance.feature.home.BalanceAdapter
-import com.ourbalance.feature.home.MainViewModel
 import com.ourbalance.feature.information.InformationActivity
+import com.ourbalance.feature.view.AddBalanceBottomSheet
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -24,7 +24,6 @@ class BalanceListFragment : Fragment() {
     private var _binding: FragmentBalanceListBinding? = null
     private val binding get() = requireNotNull(_binding)
     private val balanceListViewModel by viewModels<BalanceListViewModel>()
-    private val homeViewModel by activityViewModels<MainViewModel>()
     private val adapter by lazy { BalanceAdapter() }
 
     override fun onCreateView(
@@ -49,6 +48,10 @@ class BalanceListFragment : Fragment() {
         rvBalanceList.adapter = adapter
         ibtSetting.setOnClickListener {
             startActivity(Intent(requireContext(), InformationActivity::class.java))
+        }
+        fabAdd.setOnClickListener {
+            AddBalanceBottomSheet.newInstance()
+                .show(parentFragmentManager, ADD_BALANCE_BOTTOM_SHEET)
         }
     }
 
---
+++ feature/src/main/java/com/ourbalance/feature/view/AddBalanceBottomSheet.kt
@@ -0,0 +1,37 @@
+package com.ourbalance.feature.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.ourbalance.feature.databinding.BottomSheetAddBalanceBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddBalanceBottomSheet : BottomSheetDialogFragment() {
+    private var _binding: BottomSheetAddBalanceBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BottomSheetAddBalanceBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = AddBalanceBottomSheet()
+    }
+}

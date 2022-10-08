4c7387527e7679189b943c559e773cb24060d984
oreocube <oreocube13@gmail.com>
2022-10-08 10:37:46 +0900

    feat: 프래그먼트-뷰모델 연결 #32

--- feature/src/main/java/com/ourbalance/feature/addpayment/AddPaymentActivity.kt
+++ feature/src/main/java/com/ourbalance/feature/addpayment/AddPaymentActivity.kt
@@ -3,30 +3,84 @@ package com.ourbalance.feature.addpayment
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.ourbalance.domain.model.BalanceDetail
 import com.ourbalance.feature.R
+import com.ourbalance.feature.constant.*
 import com.ourbalance.feature.databinding.ActivityAddPaymentBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AddPaymentActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var factory: AddPaymentViewModelFactory
     private var _binding: ActivityAddPaymentBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val viewModel: AddPaymentViewModel by viewModels {
+        AddPaymentViewModel.provideFactory(
+            assistedFactory = factory,
+            balanceDetail = intent.getSerializableExtra(BALANCE_DETAIL) as BalanceDetail
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityAddPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
-                add<ChoosePayerFragment>(R.id.fcv_payment, "payer")
+                add<ChoosePayerFragment>(R.id.fcv_payment, STEP_CHOOSE_PAYER)
             }
         }
+
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModel.nextEvent.flowWithLifecycle(lifecycle).onEach {
+            supportFragmentManager.commit {
+                when (it) {
+                    is PaymentUiState.PayerSelected -> {
+                        replace<SpecifyAmountFragment>(R.id.fcv_payment, STEP_SPECIFY_AMOUNT)
+                        addToBackStack(null)
+                    }
+                    is PaymentUiState.AmountSpecified -> {
+                        replace<SpecifyContentFragment>(R.id.fcv_payment, STEP_SPECIFY_CONTENT)
+                        addToBackStack(null)
+                    }
+                    is PaymentUiState.ContentSpecified -> {
+                        replace<SpecifyDateFragment>(R.id.fcv_payment, STEP_SPECIFY_DATE)
+                        addToBackStack(null)
+                    }
+                    is PaymentUiState.Completed -> {
+                        replace<ConfirmationFragment>(R.id.fcv_payment, STEP_CONFIRMATION)
+                        addToBackStack(null)
+                    }
+                    else -> {}
+                }
+            }
+        }.launchIn(lifecycleScope)
+
+        viewModel.prevEvent.flowWithLifecycle(lifecycle).onEach {
+            supportFragmentManager.popBackStack()
+        }.launchIn(lifecycleScope)
+
+        viewModel.closeEvent.flowWithLifecycle(lifecycle).onEach {
+            finish()
+        }.launchIn(lifecycleScope)
     }
 
     companion object {
--- feature/src/main/java/com/ourbalance/feature/addpayment/ConfirmationFragment.kt
+++ feature/src/main/java/com/ourbalance/feature/addpayment/ConfirmationFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.ourbalance.feature.databinding.FragmentConfirmationBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ConfirmationFragment : Fragment() {
 
     private var _binding: FragmentConfirmationBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val viewModel by activityViewModels<AddPaymentViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +27,8 @@ class ConfirmationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
     override fun onDestroyView() {

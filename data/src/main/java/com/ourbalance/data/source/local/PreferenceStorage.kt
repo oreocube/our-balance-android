b59ab960f1d9945d31330422f7d4f774b75eef0f
oreocube <oreocube13@gmail.com>
2022-10-12 22:33:00 +0900

    add: 로컬 저장소에 닉네임 추가 #48

--- data/src/main/java/com/ourbalance/data/source/local/PreferenceStorage.kt
+++ data/src/main/java/com/ourbalance/data/source/local/PreferenceStorage.kt
@@ -4,5 +4,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface PreferenceStorage {
     val userToken: Flow<String>
+    val username: Flow<String>
     suspend fun updateToken(token: String)
+    suspend fun updateUsername(name: String)
 }

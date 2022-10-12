280db79e92a555fc0a8e236ff5d2e7b7b83df40b
oreocube <oreocube13@gmail.com>
2022-10-12 10:53:24 +0900

    chore: ktlint 체크 #42

--- feature/src/main/java/com/ourbalance/feature/screen/information/greeting/login/LoginViewModel.kt
+++ feature/src/main/java/com/ourbalance/feature/screen/information/greeting/login/LoginViewModel.kt
@@ -6,7 +6,12 @@ import com.ourbalance.domain.usecase.SignInUserUseCase
 import com.ourbalance.domain.usecase.ValidateEmailUseCase
 import com.ourbalance.domain.usecase.ValidatePasswordUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject

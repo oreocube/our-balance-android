6dd1508c9e7c95a66f2856bc3d7851e827e638e1
oreocube <oreocube13@gmail.com>
2022-10-12 22:36:42 +0900

    feat: 로그인 여부 체크 유스케이스 제거 #48

--- data/src/main/java/com/ourbalance/data/source/remote/auth/AuthDataSource.kt
+++ data/src/main/java/com/ourbalance/data/source/remote/auth/AuthDataSource.kt
@@ -2,10 +2,13 @@ package com.ourbalance.data.source.remote.auth
 
 import com.ourbalance.domain.model.auth.UserInfo
 import com.ourbalance.domain.result.Result
+import kotlinx.coroutines.flow.Flow
 
 interface AuthDataSource {
     suspend fun login(userInfo: UserInfo): Result<Unit>
     suspend fun signup(userInfo: UserInfo): Result<Unit>
     suspend fun saveToken(token: String)
     suspend fun getToken(): String
+    suspend fun isLogin(): Flow<Boolean>
+    suspend fun logout()
 }
